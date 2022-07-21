5a058f96c433be035117b1c847c8767368d4c10d
Maalikq <qureshizohan75@gmail.com>
2022-07-22 01:50:43 +0530

    completed

---
+++ component/navber.js
@@ -0,0 +1,14 @@
+let navbar=()=>{
+    return `<div>
+    <a href="index.html">
+        <img id="DNA" src="https://english.cdn.zeenews.com/static/apprun/dna/icons/logodna.png" alt="">
+    </a> 
+    <a id="hindi" href="">हिंदी में पढ़ें</a>
+    </div>
+    <a href="/">
+        <img onclick="searchbar()" id="searchIcon" src="https://english.cdn.zeenews.com/static/public/menusearch.svg" width="24" alt="Search Icon">
+    </a>
+    `
+}
+
+export default navbar
