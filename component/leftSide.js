9b4aa6be445aa42c82682739948169a67386a1ed
Raushan Raj <raushanraj901@gmail.com>
2022-07-21 17:37:54 +0530

    added navbar left_sidepage and footer

---
+++ component/leftSide.js
@@ -0,0 +1,17 @@
+let left_side=()=>{
+    return ` <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-latestnews.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Latest News</div></a>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-photos.svg" alt="menu icon" width="25"></span><a id="menuList" href="/photo-gallery">Photos</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-videos.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Videos</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-explainer.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Explainers</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-india.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">India</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-entertainment.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Entertainment</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-sports.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Sports</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-viralNews.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Viral News</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-lifestyle.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Lifestyle</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-business.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Business</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-world.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">World</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/ezmall-icon1.png" alt="menu icon" width="25"></span><a id="menuList" href="/" target="_blank">Ezmall</a></div>
+    `
+}
+
+export default left_side
