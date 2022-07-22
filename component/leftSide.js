ebbfd9ba17884371747284e600907303a2d83d42
arijeet chowdhury <arijeetchowdhury8008@gmail.com>
2022-07-22 21:16:38 +0530

    append_done ; pull req_pending

--- component/leftSide.js
+++ component/leftSide.js
@@ -2,9 +2,9 @@ let left_side=()=>{
     return ` <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-latestnews.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Latest News</div></a>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-photos.svg" alt="menu icon" width="25"></span><a id="menuList" href="/photo-gallery">Photos</a></div>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-videos.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Videos</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-explainer.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Explainers</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-india.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">India</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-entertainment.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Entertainment</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-explainer.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_explainer_page/explainer_page.html">Explainers</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-india.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_india_page/india.html">India</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-entertainment.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_entertainment_page/entertainment.html">Entertainment</a></div>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-sports.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Sports</a></div>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-viralNews.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Viral News</a></div>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-lifestyle.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Lifestyle</a></div>
