91f7b16792220a9e16758bc978a61fc41243ad33
Raushan Raj <raushanraj901@gmail.com>
2022-07-22 16:34:44 +0530

    added some pages

---
+++ component/navdown.js
@@ -0,0 +1,12 @@
+let navdown = ()=>{
+    return `
+    <div id="Part1">
+    <p>Top News</p> 
+    </div>
+    <div id="Part2">
+        <p>Presidential Election Results 2022 LIVE: Counting underway in Room 63 of Parliament House to elect new President</p>
+    </div>
+    `
+}
+
+export default navdown
