0de0eedd168cfa23c42ea25dc8bf2d070ee841c8
rupeshgondrawar <rupeshgondrawar462@gmail.com>
2022-07-24 10:18:35 +0530

    update

--- component/leftSide.js
+++ component/leftSide.js
@@ -1,16 +1,16 @@
 let left_side=()=>{
-    return ` <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-latestnews.svg" alt="menu icon" width="25"></span><a id="menuList" href="latest.html">Latest News</div></a>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-photos.svg" alt="menu icon" width="25"></span><a id="menuList" href="photoes.html">Photos</a></div>
+    return ` <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-latestnews.svg" alt="menu icon" width="25"></span><a id="menuList" href="../latest.html">Latest News</div></a>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-photos.svg" alt="menu icon" width="25"></span><a id="menuList" href="./photoes.html">Photos</a></div>
 
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-videos.svg" alt="menu icon" width="25"></span><a id="menuList" href="videos.html">Videos</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-explainer.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_explainer_page/explainer_page.html">Explainers</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-india.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_india_page/india.html">India</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-entertainment.svg" alt="menu icon" width="25"></span><a id="menuList" href="aj_entertainment_page/entertainment.html">Entertainment</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-sports.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Sports</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-viralNews.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Viral News</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-lifestyle.svg" alt="menu icon" width="25"></span><a id="menuList" href="/">Lifestyle</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-business.svg" alt="menu icon" width="25"></span><a id="menuList" href="Business_page/business.html">Business</a></div>
-    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-world.svg" alt="menu icon" width="25"></span><a id="menuList" href="World_page/world.html">World</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-videos.svg" alt="menu icon" width="25"></span><a id="menuList" href="../videos.html">Videos</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-explainer.svg" alt="menu icon" width="25"></span><a id="menuList" href="../aj_explainer_page/explainer_page.html">Explainers</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-india.svg" alt="menu icon" width="25"></span><a id="menuList" href="../aj_india_page/india.html">India</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-entertainment.svg" alt="menu icon" width="25"></span><a id="menuList" href="../aj_entertainment_page/entertainment.html">Entertainment</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-sports.svg" alt="menu icon" width="25"></span><a id="menuList" href="sport page/sport.html">Sports</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-viralNews.svg" alt="menu icon" width="25"></span><a id="menuList" href="../viral news/viralnews.html">Viral News</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-lifestyle.svg" alt="menu icon" width="25"></span><a id="menuList" href="../lifestyle page/lifestyle.html">Lifestyle</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-business.svg" alt="menu icon" width="25"></span><a id="menuList" href="../Business_page/business.html">Business</a></div>
+    <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/icons-world.svg" alt="menu icon" width="25"></span><a id="menuList" href="../World_page/world.html">World</a></div>
     <div id="menu-item"><span><img src="https://english.cdn.zeenews.com/static/public/icons/ezmall-icon1.png" alt="menu icon" width="25"></span><a id="menuList" href="https://www.ezmall.com/?utm_source=Dnaindia.com&utm_medium=Affiliate&utm_campaign=Dnaindia.com_Traffic" target="_blank">Ezmall</a></div>
 
     `
